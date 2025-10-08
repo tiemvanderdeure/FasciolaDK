d0d5908127c27b36359af7b35d343b05d8e43ac4
tiemvanderdeure <tiemvanderdeure@gmail.com>
2025-10-08 13:41:33 +0200

    utils for vsup plot

--- src/FasciolaDK.jl
+++ src/FasciolaDK.jl
@@ -3,13 +3,15 @@ module FasciolaDK
 using CSV, DataFrames, Dates, Statistics, StatsBase
 using Rasters, Rasters.Lookups, NCDatasets, GADM
 
-const datadir = "data/slagtedata"#"/home/tvd/K/slagtedata"
+const datadir = "/home/tvd/K/slagtedata"
 
 export load_registry, datadir
 export get_terraclimate
+export vsup_colormatrix, val_u_to_color, vsup_legend
 
 include("registry.jl")
 include("climate.jl")
 include("utils.jl")
+include("vsup.jl")
 
 end # module FasciolaDK

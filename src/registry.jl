755bfb70ca466c049289871f06aacef7cf4f9d9f
tiemvanderdeure <tiemvanderdeure@gmail.com>
2025-10-13 18:08:00 +0200

    read in data for udstationeringer

--- src/registry.jl
+++ src/registry.jl
@@ -7,6 +7,7 @@ function load_registry()
     beskart = CSV.read(joinpath(datadir, "beskart.csv"), DataFrame)
     bes_chr = beskart[:, [:CHRNR, :BESNR, :CHR_ID, :BES_ID]]
     besbrugsart = CSV.read(joinpath(datadir, "besbrugsart.csv"), DataFrame)
+    udstationeringer = CSV.read(joinpath(datadir, "udstationeringer.csv"), DataFrame)
 #    besbrugsart_øko = besbrugsart[findall(==(16), besbrugsart.BRUGSART_ID), :]
 
     # recode brugsarter
@@ -15,11 +16,11 @@ function load_registry()
     # drop all burgsart > 20? These are hobbydyr, avlsdyr, etc.
 
     # warns but seems to be okay?
-    koord = CSV.read(joinpath(datadir, "giskoord.csv"), DataFrame, types = Dict(:LAT => Float64, :LON => Float64))
+    koord = CSV.read(joinpath(datadir, "giskoord.csv"), DataFrame, decimal = ',', types = Dict(:LAT => Float64, :LON => Float64))
     # A couple of missing lon-lat values - these have XKOOR and YKOOR of 0
     dropmissing!(koord)
 
-    return (; dyr, dyrtilbes, slagt, fund, beskart, bes_chr, besbrugsart, koord)
+    return (; dyr, dyrtilbes, slagt, fund, beskart, bes_chr, besbrugsart, koord, udstationeringer)
 end
 
 

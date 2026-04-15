68ec55b1039303e6ef4f2cd85ae3ae7f3ac317ea
tiemvanderdeure <tiemvanderdeure@gmail.com>
2026-04-15 19:49:29 +0200

    fix path to K drive

--- src/FasciolaDK.jl
+++ src/FasciolaDK.jl
@@ -3,11 +3,10 @@ module FasciolaDK
 using CSV, DataFrames, Dates, Statistics, StatsBase
 using Rasters, Rasters.Lookups, NCDatasets, GADM
 
-const datadir = "/home/tvd/K/slagtedata"
+const datadir = "/home/tvd/K/FasciolaDK/slagtedata"
 
 export load_registry, datadir
 export get_terraclimate
-export vsup_colormatrix, val_u_to_color, vsup_legend
 
 include("registry.jl")
 include("climate.jl")
